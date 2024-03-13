1c5ef7e4b885088ab928cc58ecda10670ef86e0d
Eva Rodriguez <evarmon@ext.inditex.com>
2024-03-13 07:18:50 +0100

    fixed TextEncoder error

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
+import {rest} from 'msw'
+import {setupServer} from 'msw/node'
+import '@testing-library/jest-dom'
 import App from './App';
+
+const server = setupServer(
+  rest.get('/home-videos', (req, res, ctx) => {
+    return res(ctx.json({greeting: 'hello there'}))
+  }),
+)
+
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/home/i);
   expect(linkElement).toBeInTheDocument();
 });
